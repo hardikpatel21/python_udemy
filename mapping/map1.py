51c57ddc34c298f51ed30ff3f94b2da3febfc453
Hardik Patel <pm385@cummins.com>
2018-09-17 08:56:33 -0400

    add volcano markers and elevation info on the map

--- mapping/map1.py
+++ mapping/map1.py
@@ -1,11 +1,24 @@
 import folium
+import pandas
+
+#  getting data from the csv filr using pandas
+data = pandas.read_csv("Volcanoes_USA.csv")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
+# print(data)
+
 map = folium.Map(location=[39.209353, -85.892224], zoom_start=6, tiles="Mapbox Bright")
 
 # It is usefull to add multiple child in the map like multiple markers on the map
 fg = folium.FeatureGroup(name="My Map")
 
-for coordinates in [[39.209353, -85.892224], [40.748137, -74.060691], [40.733909, -74.061057]]:
-    fg.add_child(folium.Marker(location=coordinates, popup="Home", icon=folium.Icon(color='blue'), tooltip="This is your Home"))
+# To get the markers on my all previous homes in USA
+# for coordinates in [[39.209353, -85.892224], [40.748137, -74.060691], [40.733909, -74.061057]]:
+
+# iterate though all latitudes and longitudes in dat which we are getting from csv files
+for lt,ln,el in zip(lat, lon, elev):
+    fg.add_child(folium.Marker(location=[lt,ln], popup=str(el)+" m", icon=folium.Icon(color='blue'), tooltip="This is your Home"))
 
 
 map.add_child(fg)
